1022d4b8a03e4f33616d65dc35223ecf410f403f
adityamishra1006 <mishraaditya6001@gmail.com>
2024-04-28 03:27:02 +0530

    Login Form Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import Details from "./components/details/Details";
 import List from "./components/list/List";
 import Chat from "./components/chat/Chat";
+import Login from './components/login/Login';
+import Notification from "./components/notification/Notification";
 
 const App = () => {
+
+  const user = true
+
   return (
     <div className='container'>
-      <List />
-      <Chat />
-      <Details />
+    {
+      user ? (
+      <>  
+        <List />
+        <Chat />
+        <Details />
+      </>
+      ) : (
+      <Login/>
+      )
+    }
+    <Notification />
+      
     </div>
   )
 }
 
 export default App
+
+// 1:19:30 hrs
