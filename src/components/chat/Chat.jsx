a0d74967ac5f83c74c425fc5a5d52c3efee7c1ea
adityamishra1006 <mishraaditya6001@gmail.com>
2024-04-27 19:31:44 +0530

    Details Section Created

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,16 @@
 import "./chat.css";
 import EmojiPicker from  "emoji-picker-react";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Chat = () => {
     const [open, setOpen] = useState(false);
     const [text, setText] = useState("");
+
+    const endRef = useRef(null);
+
+    useEffect(() => {
+        endRef.current?.scrollIntoView({ behavior: "smooth" });
+    },[])
 
     const handleEmoji = e =>{
         setText((prev) => prev + e.emoji);
@@ -117,6 +123,7 @@ const Chat = () => {
                         <span>1 mins ago</span>
                     </div>
                 </div>
+                <div ref={endRef}></div>
             </div>
             <div className="bottom">
                 <div className="icons">
--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -2,7 +2,67 @@ import "./details.css";
 
 const Details = () => {
     return (
-        <div className="details">Details</div>
+        <div className="details">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Ayush</h2>
+                <p>"Focus, achieve, repeat"</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Setting</span>
+                        <img src="./arrowUp.png" alt=""/>
+                    </div>
+                </div>
+
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & Help</span>
+                        <img src="./arrowUp.png" alt=""/>
+                    </div>
+                </div>
+
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Photos</span>
+                        <img src="./arrowDown.png" alt=""/>
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">   
+                                <img 
+                                src="https://thumbs.dreamstime.com/z/panda-say-hii-cartoon-illustration-hi-white-background-57173885.jpg"  alt=""/>
+                                <span>IMG_2024_01.png</span> 
+                            </div>
+                            <img src="./download.png" className="icon" alt=""/>
+                        </div>
+
+                        <div className="photoItem">
+                            <div className="photoDetail">   
+                                <img 
+                                src="https://thumbs.dreamstime.com/z/panda-say-hii-cartoon-illustration-hi-white-background-57173885.jpg"  alt=""/>
+                                <span>IMG_2024_01.png</span> 
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+
+                        
+                    </div>
+                </div>
+                
+
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt=""/>
+                    </div>
+                </div>
+                <button>Block User</button>
+                <button className="logout">Log Out</button>
+
+            </div>
+        </div>
     )
 }
 
